d21ae95fbb336868cf8972ae38e84763ee690653
yhidetoshi <hyajima.1014@gmail.com>
2019-09-22 17:21:38 +0900

    mod json stucture

---
+++ handler/metal.go
@@ -0,0 +1,106 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/labstack/echo"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+/*
+{
+	"time": "2019-09-22T09:51:57.440183+09:00",
+	"Gold": {
+		"retailTax": 5674,
+		"purchaseTax": 5588
+	},
+	"Platinum": {
+		"retailTax": 5674,
+		"purchaseTax": 5588
+	}
+}
+*/
+
+var (
+	targetUrl = "https://gold.tanaka.co.jp/commodity/souba/index.php"
+)
+
+const (
+	timezone = "Asia/Tokyo"
+	offset   = 9 * 60 * 60
+)
+
+type Metal struct {
+	Date     time.Time `json:"time"`
+	GoldInfo GoldInfo  `json:"goldInfo"`
+	Platinum Platinum  `json:"platinum"`
+}
+
+type GoldInfo struct {
+	RetailTax   int `json:"retailTax"`
+	PurchaseTax int `json:"purchaseTax"`
+}
+
+type Platinum struct {
+	RetailTax   int `json:"retailTax"`
+	PurchaseTax int `json:"purchaseTax"`
+}
+
+func FetchMetal() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		var goldRetailTax, goldPurchaseTax, platinumRetailTax, platinumPurchaseTax string
+		jst := time.FixedZone(timezone, offset)
+		nowTime := time.Now().In(jst)
+
+		doc, err := goquery.NewDocument(targetUrl)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Fetch gold and platinum price
+		doc.Find("#metal_price_sp").Each(func(_ int, s *goquery.Selection) {
+			// Gold
+			goldRetailTax = s.Children().Find("td.retail_tax").First().Text()
+			goldPurchaseTax = s.Children().Find("td.purchase_tax").First().Text()
+			// Platinum
+			platinumRetailTax = s.Children().Find("td.retail_tax").Eq(1).Text()
+			platinumPurchaseTax = s.Children().Find("td.purchase_tax").Eq(1).Text()
+		})
+
+		// Format
+		strGoldRetailTax := strings.Replace(goldRetailTax[0:5], ",", "", -1)
+		strGoldPurchaseTax := strings.Replace(goldPurchaseTax[0:5], ",", "", -1)
+		strPlatinumRetailTax := strings.Replace(platinumRetailTax[0:5], ",", "", -1)
+		strPlatinumPurchaseTax := strings.Replace(platinumPurchaseTax[0:5], ",", "", -1)
+
+		// Convert string to int
+		intGoldRetailTax, _ := strconv.Atoi(strGoldRetailTax)
+		intGoldPurchaseTax, _ := strconv.Atoi(strGoldPurchaseTax)
+		intPlatinumRetailTax, _ := strconv.Atoi(strPlatinumRetailTax)
+		intPlatinumPurchaseTax, _ := strconv.Atoi(strPlatinumPurchaseTax)
+
+		// Set value to json
+		resMetal := Metal{
+			Date: nowTime,
+			GoldInfo: GoldInfo{
+				RetailTax:   intGoldRetailTax,
+				PurchaseTax: intGoldPurchaseTax,
+			},
+			Platinum: Platinum{
+				RetailTax:   intPlatinumRetailTax,
+				PurchaseTax: intPlatinumPurchaseTax,
+			},
+		}
+
+		bytesDurationBytesJSON, err := json.Marshal(resMetal)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return c.String(http.StatusOK, string(bytesDurationBytesJSON))
+	}
+}
--- main.go
+++ main.go
@@ -5,33 +5,38 @@ import (
 	"github.com/labstack/echo/middleware"
 	"google.golang.org/appengine"
 	"net/http"
+
+	// GAE
 	"github.com/yhidetoshi/GoEchoGAE/handler"
+	// local only
+	//"./handler"
 )
 
+// GAE
 var e = createMux()
 
 func main() {
 
 	// Echoインスタンス作成
 	e := echo.New()
+	// GAE
 	http.Handle("/", e)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.Gzip())
-	//e.Use(UseAppEngine)
-
 
 	// ルーティング
 	e.GET("/metal", handler.FetchMetal())
 
-	// サーバー起動
+	// サーバー起動 at local
 	//e.Start(":1323")
+
+	// GAE
 	appengine.Main()
 }
 
-
+// GAE
 func createMux() *echo.Echo {
 	e := echo.New()
-	//http.Handle("/", e)
 	return e
 }
