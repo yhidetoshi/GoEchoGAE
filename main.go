8324192d7ad573e346d7fb026c9db7d752010782
yhidetoshi <hyajima.1014@gmail.com>
2019-09-22 15:22:44 +0900

    gaeで実行

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
-	"./handler"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"google.golang.org/appengine"
+	"net/http"
+	"github.com/yhidetoshi/GoEchoGAE/handler"
 )
+
+var e = createMux()
 
 func main() {
 
 	// Echoインスタンス作成
 	e := echo.New()
+	http.Handle("/", e)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
+	//e.Use(UseAppEngine)
 
 
 	// ルーティング
 	e.GET("/metal", handler.FetchMetal())
 
 	// サーバー起動
-	e.Start(":1323")
+	//e.Start(":1323")
+	appengine.Main()
+}
+
+
+func createMux() *echo.Echo {
+	e := echo.New()
+	//http.Handle("/", e)
+	return e
 }
